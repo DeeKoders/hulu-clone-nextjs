0dced298d89e676109971177162dc3f9bb543c97
danyal-deekoders <deekoders@gmail.com>
2022-11-27 18:50:44 +0500

    a

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 function HeaderIcon({ Icon, title }) {
   return (
-    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-12 hover:text-white">
-      <Link href={`/${title === "Home" ? "/" : title}`}>
+    <Link href={`/${title === "Home" ? "/" : title}`}>
+      <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-12 hover:text-white">
         <Icon className="group-hover:animate-bounce h-8 mb-1" />
-      </Link>
-      <p className="opacity-0 group-hover:opacity-100 tracking-widest">
-        {title}
-      </p>
-    </div>
+        <p className="opacity-0 group-hover:opacity-100 tracking-widest">
+          {title}
+        </p>
+      </div>
+    </Link>
   );
 }
 
--- pages/Account.js
+++ pages/Account.js
@@ -1,11 +1,23 @@
 import Header from "../components/Header";
 import Nav from "../components/Nav";
 import { UserIcon } from "@heroicons/react/24/outline";
-const Account = () => {
+const Account = ({ results }) => {
+  const data = [
+    { id: 1, title: "First Name", value: "Danyal" },
+    { id: 2, title: "Last Name", value: "Ahmad" },
+    { id: 3, title: "Gender", value: "Male" },
+    { id: 4, title: "Contact No", value: "+92 312 7373848" },
+    {
+      id: 5,
+      title: "Permanent Address",
+      value: "ProductBox, Zaman Manzil, Warsak Road Peshawar",
+    },
+    { id: 6, title: "Email Address", value: "danyal.ahmad@productbox.dev" },
+    { id: 7, title: "Date of Birth", value: "26 October" },
+  ];
   return (
     <div>
       <Header />
-      {/* <Nav /> */}
       <div className="p-8 w-full md:p-4 mt-4 md:h-auto ">
         <div className=" bg-[#082e38] border border-gray-400 p-3 shadow-sm md:h-auto rounded-sm">
           <div className="justify-center md:justify-start my-5 flex items-center space-x-2 font-semibold text-gray-300 leading-8  ">
@@ -16,46 +28,14 @@ const Account = () => {
           </div>
           <div className="text-gray-300 ">
             <div className="grid md:grid-cols-1 text-lg w-full">
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">First Name</div>
-                <div className="px-4 py-2">Danyal</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Last Name</div>
-                <div className="px-4 py-2">Ahmad</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Gender</div>
-                <div className="px-4 py-2">Male</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Contact No.</div>
-                <div className="px-4 py-2">+92 312 7373848</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Current Address</div>
-                <div className="px-4 py-2">
-                  ProductBox, Zaman Manzil, Warsak Road Peshawar
-                </div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Permanant Address</div>
-                <div className="px-4 py-2">
-                  ProductBox, Zaman Manzil, Warsak Road Peshawar
-                </div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Email</div>
-                <div className="px-4 py-2">
-                  <a className="text-blue-300" href="mailto:jane@example.com">
-                    danyal.ahmad@productbox.dev
-                  </a>
-                </div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Date of Birth</div>
-                <div className="px-4 py-2">Oct 26, 2000</div>
-              </div>
+              {data.map((item) => {
+                return (
+                  <div key={item.id} className="grid grid-cols-2">
+                    <div className="px-4 py-2 font-semibold">{item.title}</div>
+                    <div className="px-4 py-2">{item.value}</div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
